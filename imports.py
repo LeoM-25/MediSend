c08c92ccd004af3ab9d980732dd8e76907be801e
LeoM-25 <leomalone@icloud.com>
2025-11-01 13:53:16 +0000

    Create imports.py

---
+++ imports.py
@@ -0,0 +1,21 @@
+import sqlite3
+import csv
+import datetime
+import time
+import random
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.popup import Popup
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty, ListProperty
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.checkbox import CheckBox
