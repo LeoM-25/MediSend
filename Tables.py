bbae3077187aab0baf2ed43d026534291b3df7c6
Fin123456789 <finlaygood81@gmail.com>
2025-11-03 11:46:25 +0000

    Update Tables.py

--- Tables.py
+++ Tables.py
@@ -8,12 +8,15 @@ def tables():
     CREATE TABLE IF NOT EXISTS A(
         ID INTEGER NOT NULL,
         Product TEXT PRIMARY KEY,
-        Quantity INTEGER NOT NULL);""")
+        Quantity INTEGER NOT NULL,
+        Expiry Date TEXT NOT NULL);""")
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS B(
         ID INTEGER NOT NULL,
         Product TEXT PRIMARY KEY,
-        Quantity INTEGER NOT NULL);""")
+        Quantity INTEGER NOT NULL,
+        Expiry Date TEXT NOT NULL);""")
 
     
+
