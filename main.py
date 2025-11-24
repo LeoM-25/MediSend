74764c0ef55dee3d76dc594eabbbefb6a01ad539
LeoM-25 <leomalone@icloud.com>
2025-11-24 19:57:50 +0000

    Implement medicine addition and sync features
    Added functionality for adding medicine and syncing.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from imports import *
 
 Window.size = (1000, 800)
 Window.clearcolor = (1, 1, 1)
-
+# main app
+class MediSend(App):
+    def build(self):
+        return Main()
 # Main window
 class Main(BoxLayout):
     def __init__(self, **kwargs):
@@ -10,7 +13,7 @@ class Main(BoxLayout):
         # title text area
         self.title_label = Label(text='MediSend', bold=True, font_size='24sp', size_hint_y=None, height=45,color=(0, 0, 0, 1))
         self.add_widget(self.title_label)
-        sync_text = 'Last synced 12:09'
+        sync_text = 'Last Synced 16:38'
         # Sync Bar
         sync_bar = BoxLayout(orientation='horizontal', size_hint_y=None, height=10, spacing=10, padding=10)
         sync_bar.add_widget(Label(text=sync_text, color=(0, 0, 0, 1)))
@@ -26,15 +29,16 @@ class Main(BoxLayout):
         grid = GridLayout(cols=5, size_hint_y=None, spacing=20, padding=10)
         grid.bind(minimum_height=grid.setter('height'))
 
-        main_content = BoxLayout(orientation='vertical', size_hint_y=1, height=750, spacing=5, padding=10)
-        # Column headers for tble
+        main_content = BoxLayout(orientation='vertical', size_hint_y=1, height=900, spacing=5, padding=10)
+        # Column headers
         headers = ["Product Name","Expiry Date", "Quantity", "Dosage", "Location"]
         for header in headers:
             grid.add_widget(Label(text=header, bold=True, color=(0, 0, 0, 1)))
-            
-        # data, placeholder
+
+    # data, placeholder
         stock = [
             ["Paracetamol","05/11/25","2", "500mg", "Hertford"],
+            ["Paracetamol", "03/11/25", "3", "500mg", "Hertford"]
         ]
 
         for row in stock:
@@ -48,14 +52,52 @@ class Main(BoxLayout):
         bottom_bar = BoxLayout(orientation='horizontal', size_hint_y=None, height=50, spacing=10, padding=5)
         self.add_button = Button(text='Add Item')
         bottom_bar.add_widget(self.add_button)
-        #self.add_button.bind(on_press=add_medicine_popup)
-        bottom_bar.add_widget(Button(text='Sync'))
-        bottom_bar.add_widget(Button(text='Help'))
+        self.add_button.bind(on_press=self.add_medicine_popup)
+        self.sync_button = Button(text='Sync')
+        self.sync_button.bind(on_press=self.sync_popup)
+        bottom_bar.add_widget(self.sync_button)
+        self.help_button = Button(text='Help')
+        bottom_bar.add_widget(self.help_button)
         self.add_widget(bottom_bar)
 
-# main app
-class MediSend(App):
-    def build(self):
-        return Main()
-# starts running main app
+    def add_medicine_popup(self, *args): # Add medicine area
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
+        layout.add_widget(Label(text='Enter medicine details:',bold=True))
+        layout.add_widget(TextInput(hint_text='Name'))
+        layout.add_widget(TextInput(hint_text='Expiry Date'))
+        layout.add_widget(TextInput(hint_text='Quantity'))
+        layout.add_widget(TextInput(hint_text='Dosage'))
+        layout.add_widget(TextInput(hint_text='Location'))
+
+        button_layout = BoxLayout(orientation='horizontal', spacing=1,padding=10)
+
+        close_btn = Button(text='Close', size_hint_y=None, height=40)
+        button_layout.add_widget(close_btn)
+
+        add_to_my_stock_btn = Button(text='Add item to stock', size_hint_y=None, height=40)
+        button_layout.add_widget(add_to_my_stock_btn)
+
+        add_to_medisend_db = Button(text='Add to MediSend database', size_hint_y=None, height=40)
+        button_layout.add_widget(add_to_medisend_db)
+
+        layout.add_widget(button_layout)
+
+        popup = Popup(title='Add Medicine',
+                      content=layout,
+                      size_hint=(0.7, 0.7))
+
+        close_btn.bind(on_press=popup.dismiss)
+        popup.open()
+
+    def sync_popup(self, *args): # Add medicine area
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        close_btn = Button(text='Close', size_hint_y=None, height=40)
+        layout.add_widget(close_btn)
+        popup = Popup(title='',
+                      content=layout,
+                      size_hint=(0.7, 0.7))
+        close_btn.bind(on_press=popup.dismiss)
+        popup.open()
+
 MediSend().run()
