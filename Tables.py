c7597132f5c28e611320eb9eac65b63b69d76ffe
LeoM-25 <leomalone@icloud.com>
2026-01-04 14:57:48 +0000

    Rename function and update table names in SQLite

--- Tables.py
+++ Tables.py
@@ -1,18 +1,18 @@
-def tables():
+def create_tables():
     import sqlite3
 
     with sqlite3.connect("MediSend.db")as db:
         cursor=db.cursor()
     
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS A(
+    CREATE TABLE IF NOT EXISTS Pharmacy_A(
         ID INTEGER NOT NULL,
         Product TEXT NOT NULL,
         Quantity INTEGER NOT NULL,
         Expiry_Date TEXT NOT NULL);""")
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS B(
+    CREATE TABLE IF NOT EXISTS Pharmacy_B(
         ID INTEGER NOT NULL,
         Product TEXT NOT NULL,
         Quantity INTEGER NOT NULL,
