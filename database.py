c4756bb5b1b6ccfbdd4367b15a38a3051fd5e24c
LeoM-25 <leomalone@icloud.com>
2026-01-05 18:04:48 +0000

    Add database table creation for Pharmacy_A and Pharmacy_B

--- database.py
+++ database.py
@@ -1,1 +1,23 @@
+import sqlite3
 
+DB_NAME = "MediSend.db" # In case we change the name of the DB
+
+def create_tables():
+    with sqlite3.connect(DB_NAME) as db:
+        cursor = db.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Pharmacy_A(
+                ID INTEGER, 
+                Product TEXT, 
+                Quantity INTEGER, 
+                Expiry_Date TEXT
+            )
+        """)
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Pharmacy_B(
+                ID INTEGER, 
+                Product TEXT, 
+                Quantity INTEGER, 
+                Expiry_Date TEXT
+            )
+        """)
