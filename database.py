40251d86950ce91d8f5af8ea2bf59899b9e0d7e1
LeoM-25 <leomalone@icloud.com>
2026-01-05 19:03:37 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+# All Fin's original code as subprograms
 DB_NAME = "MediSend.db" # In case we change the name of the DB
 
 def create_tables():
@@ -21,3 +21,23 @@ def create_tables():
                 Expiry_Date TEXT
             )
         """)
+
+def find_item(barcode): # inputs barcode and outputs location + expiry
+    locations = ["A", "B"]
+    found = []
+
+    with sqlite3.connect(DB_NAME) as db:
+        cursor = db.cursor()
+
+        for loc in locations:
+            cursor.execute(
+                f"SELECT DISTINCT Expiry_Date FROM Pharmacy_{loc} WHERE ID = ?",
+                (barcode,)
+            )
+            results = cursor.fetchall()
+
+            for (expiry,) in results:
+                found.append({"location": loc,"expiry": expiry})
+
+    return found
+
